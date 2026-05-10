08123a772865fcb43f3890eefc8329fd661383de
arman <armanganteng3057@gmail.com>
2026-05-10 22:20:41 +0800

    Fix: Tambah daftarTeman biar gak error

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+// 1. BIKIN DULU KOTAK DATANYA
 let daftarTeman = [
   { nama: "Arman", umur: 19 },
   { nama: "Nabila", umur: 17 },
   { nama: "Irma", umur: 25 }
 ];
 
+// 2. MESIN KARTU NAMA
 function sapaUser(nama, umur) {
   console.log("--- Kartu Nama ---");
   console.log("Halo bro", nama);
@@ -17,6 +19,7 @@ function sapaUser(nama, umur) {
   console.log("------------------");
 }
 
+// 3. LOOP: BACA SEMUA ORANG DI KOTAK
 for (let i = 0; i < daftarTeman.length; i++) {
   sapaUser(daftarTeman[i].nama, daftarTeman[i].umur);
 }
